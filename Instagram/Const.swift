7b11e0298de976e7fb326d09a86eb9cb8c33b5ba
syogo-user <s.photo.trumpet.005@gmail.com>
2020-02-02 14:05:40 +0900

    カリキュラム完成

---
+++ Instagram/Const.swift
@@ -0,0 +1,13 @@
+//
+//  Const.swift
+//  Instagram
+//
+//  Created by 小野寺祥吾 on 2020/01/29.
+//  Copyright © 2020 syogo-user. All rights reserved.
+//
+
+import Foundation
+struct Const{
+    static let ImagePath = "images"
+    static let PostPath = "posts"
+}
